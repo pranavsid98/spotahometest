5ed1dd24893e7274f2b75672e59003eef1077005
Pranav Siddharth <pranavsid98@gmail.com>
2019-01-30 14:03:53 +0530

    Angular App

---
+++ angularapp/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularapp/src/app/app.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http } from "@angular/http";
+import { CanActivate, Router } from '@angular/router';
+
+import 'rxjs/add/operator/toPromise';

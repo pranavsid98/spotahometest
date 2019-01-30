fd8c9732fd8baa9d1664328a183dbd1903ca36e7
Pranav Siddharth <pranavsid98@gmail.com>
2019-01-30 13:52:51 +0530

    Settings file

--- spotahometest/settings.py
+++ spotahometest/settings.py
@@ -14,6 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ANGULAR_APP_DIR = os.path.join(BASE_DIR, 'angularapp/dist')
+STATIC_DIR = os.path.join(BASE_DIR, 'home_static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,4 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(ANGULAR_APP_DIR),
+    os.path.join(STATIC_DIR)
+]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
